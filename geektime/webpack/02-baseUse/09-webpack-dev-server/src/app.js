cb6312fafbfeae4cd8db2645fc2ad0055e1b3f2d
yijiangwang <983148317@qq.com>
2020-04-19 20:58:32 +0800

    热更新

---
+++ geektime/webpack/02-baseUse/09-webpack-dev-server/src/app.js
@@ -0,0 +1,16 @@
+// @ts-check
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Yjw extends React.Component {
+  render() { 
+    return (<div>
+      Hello Webpack-dev-server
+    </div>);
+  }
+}
+ 
+ReactDOM.render(
+  <Yjw />,
+  document.getElementById('root')
+);
