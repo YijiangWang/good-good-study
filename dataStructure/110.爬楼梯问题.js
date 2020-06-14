b031c96d35ed55ea3a625430c80cc7750b50015e
yijiangwang <wangyijiang@unipus.cn>
2020-06-14 21:17:50 +0800

    爬楼梯问题

---
+++ dataStructure/110.爬楼梯问题.js
@@ -0,0 +1,21 @@
+/**
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 注意：给定 n 是一个正整数。
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+  if (n<3) return n;
+  let start = 1;
+  let end = 2;
+  for(let i = 2; i < n; i++){
+    let temp = start + end;
+    start = end;
+    end = temp;
+  }
+  return end
+};
+
+console.log(climbStairs(2))
+console.log(climbStairs(3))
