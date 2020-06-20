d99e238bd0e514fb3bd081b4a57fc720f38178c7
yijiangwang <wangyijiang@unipus.cn>
2020-06-20 22:16:58 +0800

    股票最大利润

--- dataStructure/102.求股票交易获取最大的利润.js
+++ dataStructure/102.求股票交易获取最大的利润.js
@@ -3,14 +3,25 @@
 // 注意：你不能在买入股票前卖出股票。
 
 var maxProfit = function(prices) {
-  if( prices.length === 1) return 0;
-  let minPrice = prices[0];
+  // if( prices.length === 1) return 0;
+  // let minPrice = prices[0];
+  // let profit = 0;
+  // for (let i=1; i<prices.length; i++){
+  //   if(prices[i]<minPrice){
+  //     minPrice = prices[i];
+  //   }else if(prices[i]-minPrice>profit){
+  //     profit = prices[i]-minPrice;
+  //   }
+  // }
+  if (prices.length === 0) return 0
   let profit = 0;
-  for (let i=1; i<prices.length; i++){
-    if(prices[i]<minPrice){
-      minPrice = prices[i];
-    }else if(prices[i]-minPrice>profit){
-      profit = prices[i]-minPrice;
+  let minValue = prices[0]
+  for (let i = 0; i < prices.length; i++) {
+    const element = prices[i];
+    if (element < minValue){
+      minValue = element;
+    } else {
+      profit = Math.max(element - minValue, profit)
     }
   }
   return profit;
