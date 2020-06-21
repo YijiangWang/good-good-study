b75d0aac09c4cf4adccdba5c96f6374ccfc84fa3
yijiangwang <wangyijiang@unipus.cn>
2020-06-21 15:20:59 +0800

    买卖股票的最佳时机

--- dataStructure/120.买卖股票最佳时机2.js
+++ dataStructure/120.买卖股票最佳时机2.js
@@ -3,13 +3,14 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-  let maxprofit = 0;
+  if (prices.length === 0) return 0;
+  let maxProfit = 0;
   for (let i = 1; i < prices.length; i++) {
-    if (prices[i] > prices[i - 1])
-      maxprofit += prices[i] - prices[i - 1];
+    const element = prices[i];
+    maxProfit += Math.max(prices[i] - prices[i-1], 0);
   }
-  return maxprofit;
+  return maxProfit;
 };
 
 // [7,1,5,3,6,4]
-console.log(maxProfit([1, 2, 3, 4, 5]))
+console.log(maxProfit([7,6,4,3,1]))
