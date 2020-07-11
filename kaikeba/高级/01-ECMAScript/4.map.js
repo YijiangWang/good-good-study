9a5fd7632a099755edc32d635945499db8e22b69
yijiangwang <983148317@qq.com>
2020-07-11 14:00:01 +0800

    es6中的map学习

--- kaikeba/高级/01-ECMAScript/4.map.js
+++ kaikeba/高级/01-ECMAScript/4.map.js
@@ -6,12 +6,12 @@ let arr = [
 ];
 
 let m = new Map(arr);
-console.log(m, m.size);
+console.log(m, m.size); // Map(4) { 'a' => 1, 'b' => 2, 'c' => 3, 'd' => 4 } 4
 
 // console.log(m.clear());  // undefined
 // console.log(m.delete('a')); // true
 // console.log(m.has('a'));  // true
 // console.log(m.get('a'));  // 1
-console.log(m.set('f', 8));  // 返回一个新的 Map，可以进行链式调用
+console.log(m.set('f', 8));  // 返回一个新的 Map，可以进行链式调用, Map(5) { 'a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'f' => 8 }
 
 console.log(m, m.size);
